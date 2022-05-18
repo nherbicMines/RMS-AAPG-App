73a7a35281151b29507c3cc23fdbf1eb05c2ec73
Nick <nherbic@mymail.mines.edu>
2022-05-18 17:50:18 -0600

    Properly setup React Router

--- cry/src/index.js
+++ cry/src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render((
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  ), document.getElementById('root'));
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
