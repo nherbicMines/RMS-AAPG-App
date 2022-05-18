f3853b589e395800de6b0111322e0d36721180bc
Nick <nherbic@mymail.mines.edu>
2022-05-18 13:35:52 -0600

    Removed dummy directories

---
+++ test.js
@@ -0,0 +1,28 @@
+/*var dbConnection = SQL.connect( { Driver: "MySQL",
+    Host: "www-test",
+    Port: 3306,
+    Database: "test1.accdb",
+    UserName: "evals",
+    Password: "3vals" } );
+
+var sql = "SELECT num_reviews, organization, attractiveness, legibility, technical_originality,  \
+        significance_and_longevity, substantiation, overall_impression\
+        FROM scores";
+
+
+
+var result = dbConnection.query(sql);
+
+var org_avg = sql.organization / num_reviews;
+var org_avg = sql.organization / num_reviews;
+var org_avg = sql.organization / num_reviews;
+var org_avg = sql.organization / num_reviews;
+var org_avg = sql.organization / num_reviews;
+var org_avg = sql.organization / num_reviews;
+*/
+
+function SayHello(name) {
+    console.log("Hello " + name);
+}
+
+console.log(module);
