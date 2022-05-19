389e7f2354c6d2e88f620d9535defc633065b9a3
Mikayla <mrsherwood@mines.edu>
2022-05-19 12:42:26 -0600

    Updated the Grade Page to have a skeleton.

--- cry/src/Pages/Grade.js
+++ cry/src/Pages/Grade.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Grade() {
   return (
-    <div>Grade</div>
+    <div>
+      <p>Presentation</p>
+      
+      <label>Organization (0-20):</label>
+      <input type = "number" name = "organization"/>
+
+      <label>Attractiveness (0-10):</label>
+      <input type = "number" name = "attractiveness"/>
+
+      <label>Legibility (0-5):</label>
+      <input type = "number" name = "legibility"/>
+    </div>
   )
 }
 
