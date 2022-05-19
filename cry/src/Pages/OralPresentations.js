cb6836bef1bfc2f6bc3e8497290b290212dc87bd
lexi nelson <lexirnelson@gmail.com>
2022-05-18 22:05:35 -0600

    dropdowns in presentation picker

--- cry/src/Pages/OralPresentations.js
+++ cry/src/Pages/OralPresentations.js
@@ -3,7 +3,13 @@ import React from 'react'
 function OralPresentations() {
   return (
     <div>
-      Oral Presentations
+        Oral Presentations
+        <db></db>
+        
+        <select name="selectList" id="selectList">
+            <option value="option 1">Option 1</option>
+            <option value="option 2">Option 2</option>
+        </select>
     </div>
   )
 }
--- cry/src/Pages/PosterPresentations.js
+++ cry/src/Pages/PosterPresentations.js
@@ -4,6 +4,12 @@ function Presentations() {
   return (
     <div>
       Poster Presentations
+      <db></db>
+        
+        <select name="selectList" id="selectList">
+            <option value="option 1">Option 1</option>
+            <option value="option 2">Option 2</option>
+        </select>
     </div>
   )
 }
