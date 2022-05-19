4238d2a6d92e150ac31080412e89b834b3e43778
Mikayla <mrsherwood@mines.edu>
2022-05-19 14:21:24 -0600

    Added comments to help for further implemention.

--- cry/src/Pages/TimeSelection.js
+++ cry/src/Pages/TimeSelection.js
@@ -1,5 +1,5 @@
-import {Link } from "react-router-dom";
 import React from 'react'
+import {Link } from "react-router-dom";
 
 //Add a variable to determine PresenationType will be shown.
 /* Time Selection Page
