97704de011503970c568f1b971a126d91e048578
lexi nelson <lexirnelson@gmail.com>
2022-05-18 21:26:53 -0600

    3 buttons!!

--- cry/src/Pages/PresentationType.js
+++ cry/src/Pages/PresentationType.js
@@ -1,8 +1,33 @@
+import {Link } from "react-router-dom";
 import React from 'react'
 
 function PresentationType() {
   return (
-    <div>PresentationType</div>
+    <div>
+            <p>
+              Click on a button below.
+            </p>
+
+            <Link to="/OralPresentations"><button>
+              Oral Presentations
+            </button>
+            </Link>
+
+            <br></br>
+
+            <Link to="/PosterPresentations"><button>
+              Poster Presentations
+            </button>
+            </Link>
+
+            <br></br>
+
+            <Link to="/Schedule"><button>
+              Schedule
+            </button>
+            </Link>
+
+          </div>
   )
 }
 
