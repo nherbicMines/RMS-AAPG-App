0545696ea7db224cdc3aec433673e93fe76d6951
Mikayla <mrsherwood@mines.edu>
2022-05-19 12:13:13 -0600

    Updated the Time Selection Page

--- cry/src/Pages/TimeSelection.js
+++ cry/src/Pages/TimeSelection.js
@@ -1,8 +1,28 @@
+import {Link } from "react-router-dom";
 import React from 'react'
 
+//Add a variable to determine PresenationType will be shown.
 function TimeSelection() {
   return (
-    <div>TimeSelection</div>
+    <div>
+      <p>Time Selection</p>
+      
+      <Link to = "/PresentationType">
+        <button>Monday AM</button>
+      </Link>
+
+      <Link to = "/PresentationType">
+        <button>Monday PM</button>
+      </Link>
+
+      <Link to = "/PresentationType">
+        <button>Tuesday AM</button>
+      </Link>
+
+      <Link to = "/PresentationType">
+        <button>Tuesday PM</button>
+      </Link>
+      </div>
   )
 }
 
