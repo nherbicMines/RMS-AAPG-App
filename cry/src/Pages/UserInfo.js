57d9d67d2b3268a3f48a9a93038d94236b33c607
Josue Milenga <josuemilenga@gmail.com>
2022-05-19 14:50:08 -0600

    Added textfields to userinfo page and submit button

--- cry/src/Pages/UserInfo.js
+++ cry/src/Pages/UserInfo.js
@@ -2,7 +2,27 @@ import React from 'react'
 
 function userInfo() {
   return (
-    <div>userInfo</div>
+    <div clasName="information">
+    <p>Judging Form</p>
+    <p1>Please enter your information</p1>
+    <form>
+      <label className="textfields">
+        First Name:
+        <input type="text" name="name" />
+        Last Name: 
+        <input type="text" name="name" />
+        Company:
+        <input type="text" name="name" />
+        Email:
+        <input type="text" name="name" />
+      </label>
+
+      <div className="containButton">
+      <input type="submit" value="Submit" />
+      </div>
+
+    </form>
+    </div>
   )
 }
 
