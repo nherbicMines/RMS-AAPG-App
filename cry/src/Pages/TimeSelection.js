9fa4c2480f74a833382874f49403bde504a1afd0
Mikayla <mrsherwood@mines.edu>
2022-05-19 12:25:00 -0600

    Add comments to Time Selection

--- cry/src/Pages/TimeSelection.js
+++ cry/src/Pages/TimeSelection.js
@@ -2,6 +2,10 @@ import {Link } from "react-router-dom";
 import React from 'react'
 
 //Add a variable to determine PresenationType will be shown.
+/* Time Selection Page
+ * Function: Displays different button options to choose which day the judging will take place.
+ * All the buttons are linked to the corresponding Presentation Type Page.
+*/
 function TimeSelection() {
   return (
     <div>
