8a7cc07d8c56978b7231f37bb1f444ff571e506a
lexi nelson <lexirnelson@gmail.com>
2022-05-20 18:05:10 -0600

    both presentation types now make a dropdown menu from an array and store the user choice

--- cry/src/Pages/OralPresentations.js
+++ cry/src/Pages/OralPresentations.js
@@ -1,17 +1,52 @@
 import React from 'react'
 
+var myarray = ["", "enter","some","presentations"]
+
 function OralPresentations() {
+  
   return (
     <div>
         Oral Presentations
-        <db></db>
-        
-        <select name="selectList" id="selectList">
-            <option value="option 1">Option 1</option>
-            <option value="option 2">Option 2</option>
-        </select>
+        <br />
+      <PresentationForm />
     </div>
   )
 }
 
+class PresentationForm extends React.Component{
+  constructor(props) {
+    super(props);
+    //create ref to store selection
+    this.presentation = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    if(this.presentation.current.value === ""){
+      alert("Must pick a presentation");
+    }
+    else{
+      alert(this.presentation.current.value);
+    }
+  }
+
+  render(){
+    return (
+      <form onSubmit = {this.handleSubmit}>
+        <select ref={this.presentation}>
+            {myarray.map(data=>(
+              <option title={'data'}>{data}</option>
+            ))}
+        </select>
+        <br />
+        <input type="submit" name="Submit" />
+      </form>
+    )
+  }
+
+}
+
+
 export default OralPresentations
+
--- cry/src/Pages/PosterPresentations.js
+++ cry/src/Pages/PosterPresentations.js
@@ -1,17 +1,48 @@
 import React from 'react'
-
-function Presentations() {
-  return (
+var myarray = ["", "put", "posters", "here"]
+function PosterPresentations() {
+return (
     <div>
-      Poster Presentations
-      <db></db>
-        
-        <select name="selectList" id="selectList">
-            <option value="option 1">Option 1</option>
-            <option value="option 2">Option 2</option>
-        </select>
+        Poster Presentations
+        <br />
+      <PresentationForm />
     </div>
   )
 }
 
-export default Presentations
+class PresentationForm extends React.Component{
+  
+  constructor(props) {
+    super(props);
+    //create ref to store selection
+    this.presentation = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    if(this.presentation.current.value === ""){
+      alert("Must pick a presentation");
+    }
+    else{
+      alert(this.presentation.current.value);
+    }
+  }
+
+  render(){
+    return (
+      <form onSubmit = {this.handleSubmit}>
+        <select ref={this.presentation}>
+            {myarray.map(data=>(
+              <option title={'data'}>{data}</option>
+            ))}
+        </select>
+        <br />
+        <input type="submit" name="Submit" />
+      </form>
+    )
+  }
+
+}
+
+export default PosterPresentations
