25ebacfb419c6d816d1053e5a4da66e29146687f
r3a4 <racoronanunez@gmail.com>
2022-05-23 22:03:01 -0600

    implemented Schedule button in home page

--- cry/src/Pages/Home.js
+++ cry/src/Pages/Home.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import {Link } from "react-router-dom";
-
-
-
-
+import pdf from "../pdf/Academic-Calendar-2022-23.pdf";
 
 function Home() {
   
   return (
     <div>
-    <img src={process.env.PUBLIC_URL + "/nexthorizon.jpg"}/>
-
-    <div>
-    <label>
-      Judging Form
-    </label>
-    <br></br>
-
-    <Link to = "/UserInfo">
-    <button>Judging Form</button>
-    </Link>
-
-    </div>
+      <img src={process.env.PUBLIC_URL + "/nexthorizon.jpg"}/>
+      <div>
+        <label>
+          Judging Form
+        </label>
+        <br></br>
+        <Link to = "/UserInfo">
+          <button>Judging Form</button>
+        </Link>
+      </div>
+      <div>
+        <label>
+          Schedule
+        </label>
+        <br></br>
+        <a href={pdf} target = "_blank">
+          <button> Schedule </button>
+        </a>
+      </div>
     </div>
 
   )
--- cry/src/Pages/Schedule.js
+++ cry/src/Pages/Schedule.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import pdf from "../pdf/Academic-Calendar-2022-23.pdf";
 function Schedule() {
   return (
     <div>
-      putta schedule here
+      <a href={pdf} target = "_blank"></a>
     </div>
   )
 }
