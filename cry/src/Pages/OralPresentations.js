d65a59ab02366834d3b8591deebacf6eb0e4b48c
lexi nelson <lexirnelson@gmail.com>
2022-05-26 14:20:17 -0600

    working on presentation selection

--- cry/src/Pages/OralPresentations.js
+++ cry/src/Pages/OralPresentations.js
@@ -1,20 +1,35 @@
-import React from 'react'
-
-var myarray = ["", "enter","some","presentations"]
+import React, { Component } from 'react'
+//import Presentation from './helpers/Presentation' 
 
 function OralPresentations() {
-  
   return (
     <div>
-        Oral Presentations
-        <br />
+      Presentation Form
       <PresentationForm />
     </div>
   )
 }
 
+class Presentation{
+  constructor(title, presenterName, time){
+    this.title = title;
+    this.presenterName = presenterName;
+    this.time = time;
+  }
+}
+
+let pres1 = new Presentation("Suck my dick", "You're mom", "12:00 PM");
+let pres2 = new Presentation("Oh god", "some guy", "12:00 PM");
+let pres3 = new Presentation("Pizza time", "I.C weiner", "12:00 PM");
+let myarray = [pres1, pres2, pres3];
+
+
+
+
+
 class PresentationForm extends React.Component{
-  constructor(props) {
+  
+  /*constructor(props) {
     super(props);
     //create ref to store selection
     this.presentation = React.createRef();
@@ -29,24 +44,23 @@ class PresentationForm extends React.Component{
     else{
       alert(this.presentation.current.value);
     }
+  }*/
+  makeButton(data) {
+    return (
+        <button class="button">
+          {data.title}
+        </button>
+    );
   }
-
   render(){
     return (
-      <form onSubmit = {this.handleSubmit}>
-        <select ref={this.presentation}>
-            {myarray.map(data=>(
-              <option title={'data'}>{data}</option>
-            ))}
-        </select>
-        <br />
-        <input type="submit" name="Submit" />
-      </form>
+      <div>
+        {myarray.map(this.makeButton,this)}
+      </div>
     )
   }
 
 }
-
 
 export default OralPresentations
 
---
+++ cry/src/Pages/helpers/Presentation.js
@@ -0,0 +1,8 @@
+class Presentation{
+  constructor(title, presenterName, time){
+    this.title = title;
+    this.presenterName = presenterName;
+    this.time = time;
+  }
+}
+export default Presentation
