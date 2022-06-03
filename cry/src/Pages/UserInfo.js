208174afbc971b72321a4579376f652ab6b24093
Nick <nherbic@mymail.mines.edu>
2022-06-03 14:13:54 -0600

    Fixed UserInfo page

--- cry/src/Pages/UserInfo.js
+++ cry/src/Pages/UserInfo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import { TextField } from '@mui/material'
+import Axios from 'axios'
 
 function userInfo() {
   return (
