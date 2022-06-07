ff25671369222cc0babca8118d359be792397815
Nick <nherbic@mymail.mines.edu>
2022-06-07 12:58:37 -0600

    Made userQuery its own file

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+const userQuery = require("./userQuery");
+
 const express = require('express')
 const app = express()
 const { Connection, Request } = require("tedious");
@@ -38,43 +40,7 @@ connection.connect();
 
 
 // User Info Query
-app.post("/UserInfo", (req, res) => {
-  const firstName = req.body.firstName
-  const lastName = req.body.lastName
-  const company = req.body.company
-  const email = req.body.email
-
-  if (email == "deleteAll") {
-    const request = new Request(
-      `DELETE FROM users`,
-      (err, rowCount) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log(`${rowCount} row(s) returned`);
-        }
-      }
-    );
-    connection.execSql(request);
-  } else {
-    const request = new Request(
-      `INSERT INTO users (first_name, last_name, company, email) VALUES (@first,@last,@comp,@email)`,
-      (err, rowCount) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log(`${rowCount} row(s) returned`);
-        }
-      }
-    );
-
-    request.addParameter('first', TYPES.VarChar, firstName);
-    request.addParameter('last', TYPES.VarChar, lastName);
-    request.addParameter('comp', TYPES.VarChar, company);
-    request.addParameter('email', TYPES.VarChar, email);
-    connection.execSql(request);
-  }
-})
+userQuery(app, connection);
 
 app.listen(3001, ()=> {
   console.log("Please work")
