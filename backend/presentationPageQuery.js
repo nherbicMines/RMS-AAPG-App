782f79bb2887be46015f16c128ea5239c0196bdf
Josue Milenga <josuemilenga@gmail.com>
2022-06-09 14:39:00 -0600

    presentation query implemented, just need to testif query works

--- backend/presentationPageQuery.js
+++ backend/presentationPageQuery.js
@@ -4,17 +4,26 @@ var TYPES = require('tedious').TYPES;
 module.exports = function presentationPageQuery(app, connection){
     app.get("/Presentations", (req, res) => {
 
+        // create a new request
         const request = new Request (
-            `SELECT Title, Name, Time FROM presentations`, 
+            `SELECT Title, Name, Time FROM presentations`,    
 
+            // throw err if query failed or display the res to the console
             (err, rowCount) => {
-                if(err){
-                    console.error(err.message);
-                }else{
-                    console.log(`${rowCount} row(s) returned`);
+                if (err) {
+                  console.error(err.message);
+                } else {
+                  console.log(`${rowCount} row(s) returned`);
                 }
-            }
-        );
-        connection.execSql(request);
-    });
-}
+              }
+            );
+          
+            request.on("row", columns => {
+              columns.forEach(column => {
+                console.log("%s\t%s", column.metadata.colName, column.value);
+              });
+            });
+          
+            connection.execSql(request);
+          });
+        }
