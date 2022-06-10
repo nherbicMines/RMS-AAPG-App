1ba22e6bf9bec48bc33e41c67c8ee81401042291
lexi nelson <lexirnelson@gmail.com>
2022-06-10 12:19:31 -0600

    reworking the passing of data between pages

--- cry/src/Pages/helpers/SessionCards.js
+++ cry/src/Pages/helpers/SessionCards.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {Link, useLocation} from "react-router-dom";
-import Session from '../Session';
+
 
 //array with dummy data 
 let myarray = ["session1", "session2", "session3"];
@@ -15,7 +15,7 @@ export default class SessionCards extends Component{
 //function that kinda saves info of the button you press
   handleClick(event){
       var id = event.target.id;
-      console.log(id);
+
   }
 
   //when we start interfacing with DB, get rid of title in id 
@@ -25,24 +25,23 @@ export default class SessionCards extends Component{
   - The id for each button is associated to the presentation's key from DB
   - each button will take the user to the judge form page after clicking */
     makeButton(data) {
-        const email = this.props.email;
         return (
+          <div>
             <Link to = {"/Presentations"}
               //send the email and session string to next page
               state = {{
-                email: email,
-                session: data 
+                session: data
             }}> 
             <button class="button" id={data} onClick={this.handleClick}>
                 {data}
             </button>
             </Link>
+            </div>
         );
     }
 
   /*This returns the array input when we map make button onto each instance */
     render(){
-        //const{state} = this.props.location;
         return (
         <div>
             {myarray.map(this.makeButton,this)}
--- cry/src/Pages/session.js
+++ cry/src/Pages/session.js
@@ -1,23 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import SessionCards from './helpers/SessionCards';
 import {Link, useLocation } from "react-router-dom";
 
 //This is the session page, it simply displays the session cards class and has a back button
 function Session() {
-    const location = useLocation();
-    const { email } = location.state;
-    console.log(email);
 
     return (
     <div>
         <p class="bigText">
         Sessions 
         </p>
-        <SessionCards />
-        <Link to={"/TimeSelection"}
-          state={{
-            email: email,
-          }}>
+        <SessionCards/>
+        <Link to="/TimeSelection">
             <button class="buttonSmallLexi">
                 Back  
             </button>
