7ed2db088aac31ec0097d58ccedd82fc6e982ece
r3a4 <racoronanunez@gmail.com>
2022-06-13 09:54:33 -0600

    Created base for admin page

---
+++ cry/src/Pages/Admin.js
@@ -0,0 +1,18 @@
+import {Link, useParams } from "react-router-dom";
+import React from 'react'
+import Axios from 'axios'
+
+function Admin() {
+  return (
+    <div>
+        <p class="bigText">
+            Data
+        </p>
+        <button class="buttonSmallLexi">
+            Download 
+        </button>
+    </div>
+  )
+}
+
+export default Admin
